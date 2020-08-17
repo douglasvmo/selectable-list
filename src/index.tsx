9bdadac7011b6036996db4bf5a33b0b1381c4f3e
douglasOlv <douglasolv.dev@gmail.com>
2020-08-16 23:35:41 -0300

    :zap: create component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,35 @@
 import * as React from 'react'
-import { View, Text } from 'react-native'
+import { FlatList, FlatListProps, TouchableOpacity } from 'react-native'
 
-export default function index() {
-  return (
-    <View>
-      <Text>My first package component</Text>
-    </View>
-  )
+interface ItemObj {
+  isSelected: boolean,
+
 }
+
+interface FlatListSelectableProps extends FlatListProps<ItemObj> {
+  renderItem: any;
+}
+
+
+export const FlatListSelectable: React.FC<FlatListSelectableProps> = (props) => {
+
+  const [data, setData] = React.useState<any>(() => {
+    return props.data?.map(item => ({ ...item, isSelected: false }))
+  })
+
+
+
+  function handleToucheInItem() {
+    console.log(data)
+  }
+  function renderItem({ item }) {
+    return <TouchableOpacity onPress={handleToucheInItem}>
+      {props.renderItem()}
+    </TouchableOpacity>
+  }
+
+
+
+
+  return <FlatList {...props} data={data} renderItem={renderItem} />
+} 
