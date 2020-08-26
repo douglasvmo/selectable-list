51160f842be52c1ffafa9cf96159e5727d35dd09
Douglas Vinicius <47726255+douglasOlv@users.noreply.github.com>
2020-08-26 11:06:43 -0300

    feat: inserted a padding internal in list

--- src/index.tsx
+++ src/index.tsx
@@ -33,6 +33,7 @@ export const FlatListSelectable: React.FC<FlatListSelectableProps> = (props) => 
 
 
   return <FlatList
+    contentContainerStyle={{ paddingVertical: 5}}
     {...props}
     data={data}
     renderItem={renderItem}
