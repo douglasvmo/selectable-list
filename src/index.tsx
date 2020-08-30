8191d936e5b369df0236fce0a439d82b26ac26b6
douglasOlv <douglasolv.dev@gmail.com>
2020-08-30 16:30:34 -0300

    fead: return selected item id

--- src/index.tsx
+++ src/index.tsx
@@ -10,25 +10,44 @@ interface ItemObj extends FlatList {
 interface FlatListSelectableProps extends FlatListProps<ItemObj> {
   renderItem: any;
   styleSelectedItem?: StyleProp<ViewStyle>,
-  styleUnSelectedItem?: StyleProp<ViewStyle>
+  styleUnSelectedItem?: StyleProp<ViewStyle>,
+  selectedItemId?: Function
+
 }
+
+type DataStateType = Array<ItemObj> | undefined
 
 
 export const FlatListSelectable: React.FC<FlatListSelectableProps> = (props) => {
 
-  const [data, setData] = React.useState<any>(() => {
-    return props.data?.map((item, index) => ({ ...item, isSelected: false, id: index.toString() }))
+  const [data, setData] = React.useState<DataStateType>((): DataStateType => {
+    if (props.data) {
+      const initialValue = props.data?.map((item, index) => ({ ...item, isSelected: false, id: index.toString() }))
+      return initialValue as DataStateType
+    }
   })
 
   React.useEffect(() => {
+
+    if (props.data?.length === 0) {
+      return;
+    }
+
+    const selectedArray = data?.filter(item => item.isSelected === true)
+    const idSelectedItemArray = selectedArray?.map(item => item.id)
+
+    if (props.selectedItemId) {
+      props.selectedItemId(idSelectedItemArray)
+    }
 
   }, [data])
 
 
 
   function handleToucheInItem(itemId: string) {
-    setData((prevState) => prevState.map(item => item.id === itemId ? { ...item, isSelected: !item.isSelected } : item))
+    setData((prevState): DataStateType => prevState?.map(item => item.id === itemId ? { ...item, isSelected: !item.isSelected } : item) as DataStateType)
   }
+
   function renderItem({ item }) {
     return (
       <TouchableOpacity
